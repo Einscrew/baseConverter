33b2bf0e048ff2f58e0fcee86005cd909f9ec722
Einscrew <manel.rodrigo.reis@gmail.com>
2016-11-20 16:50:58 +0000

    Only converts decimal numbers

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*#include "func.h"*/
+
+char int_to_base(int integer, int base)
+{
+	if(integer==0)
+	{
+		return '0';
+	}
+
+	printf("%c", int_to_base(integer/base, base));
+
+	if(integer%base <= 9)
+	{
+		return '0'+integer%base;
+	}
+	else
+	{
+		return '0'+7+(integer%base);
+	}
+}
+
+int main() {
+	int num, base;
+	
+	printf("Introduza um numero em base 10 e a base de conversão [N B]:\n\t[Prima q para terminar]\n");
+
+	
+	do
+	{
+		if(scanf("%d %d", &num, &base)!= 2 || base < 2)
+		{
+			return 1;
+		}
+		printf("->");
+		printf("%c\n", int_to_base(num, base));	
+	}while(!scanf("q"));
+	
+	return 0;
+}
